69e84fb68b9d4957bd8c293939784293a0992754
Chinyere Gloria Nnanna <cgnnanna@gmail.com>
2021-10-31 20:30:45 +0100

    handled  requests to routes that do not exist

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use("/drone", droneRoute);
 
+app.use((req, res)=>{
+    res.status(404).json({
+        message: "The route you are trying to access does not exist"
+    });
+});
+
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
     console.log(`server running at port ${port}`);
