65554f874d2253d445cc5a0b7786060c315a2cc0
Chinyere Gloria Nnanna <cgnnanna@gmail.com>
2021-11-04 15:41:40 +0100

    save audit

---
+++ db/auditRepository.js
@@ -0,0 +1,18 @@
+const { auditDb } = require("./simpleJsonDbConfig");
+db = auditDb();
+
+
+const saveAudit = (name, auditObj) => {
+    let audit = getAudit(name);
+    audit = audit ? audit : [];
+    audit.push(auditObj);
+    db.set(name, audit);
+}
+
+const getAudit = (name) => {
+    return db.get(name);
+}
+
+module.exports = {
+    saveAudit
+}
